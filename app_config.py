65c4b020eceeeb4069742a4bedded94c95edd7ab
haha370104 <137368440@qq.com>
2016-04-07 15:30:09 +0800

    hide db_url

--- app_config.py
+++ app_config.py
@@ -1,11 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from db_config import db_url
 
 app = Flask(__name__)
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Ab123456@115.28.206.58:3306/Project'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Ab123456@115.28.206.58:3306/Project'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:ttkl1231@localhost:3306/Project'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@localhost:3306/Project'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-#test
