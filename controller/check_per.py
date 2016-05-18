f983f8a3c14546859b75b78ad1d0987f162934dd
haha370104 <137368440@qq.com>
2016-05-18 09:33:03 +0800

    admin message notice

--- controller/check_per.py
+++ controller/check_per.py
@@ -31,12 +31,27 @@ def driver_check_message(f):
     def decorated_function(*args, **kwargs):
         driver_ID = session['driver_account_id']
         number = message.query.filter(
-            or_(and_(message.receiver_ID == driver_ID, message.flag == True, message.read_flag == False),
-                and_(message.sender_ID == driver_ID, message.flag == False, message.read_flag == False))).all()
+            and_(message.receiver_ID == driver_ID, message.flag == True, message.read_flag == False)).all()
         length = len(number)
         if length == 0:
             length = ''
         session['message_count'] = length
+
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+def admin_check_message(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        admin_ID = session['admin_account_id']
+        number = message.query.filter(
+            and_(message.receiver_ID == admin_ID, message.flag == False, message.read_flag == False)).all()
+        length = len(number)
+        if length == 0:
+            length = ''
+        session['admin_message_count'] = length
 
         return f(*args, **kwargs)
 
